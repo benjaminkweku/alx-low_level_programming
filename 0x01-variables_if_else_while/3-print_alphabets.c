d9adf5efd11ba2fae2692cba91edf70eb902425f
benjaminkweku <benjaminqweku16@gmail.com>
2022-03-12 15:36:40 -0800

    corrected task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,25 +5,14 @@
  * Return: alway returns 0
  *
  */
-void uppercaseAlphabets()
-{
-  
-    // Run a loop from 65 to 90
-    for (int c = 65; c <= 90; ++c)
-  
-        // print its ascii values
-        printf("%c", c);
-  
-    printf("\n");
-}
 int main(void)
 {
 	char a;
 
 	for (a = 'a'; a <= 'z' ; a++)
 		putchar(a);
-		uppercaseAlphabets();
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
 	putchar('\n');
 	return (0);
 }
-
