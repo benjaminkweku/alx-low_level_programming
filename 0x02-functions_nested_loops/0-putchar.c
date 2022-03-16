cc1b0d76c2dbe7628aa5000eb13e4ff44d960fc5
benjaminkweku <benjaminqweku16@gmail.com>
2022-03-16 03:28:48 -0700

    task1 print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include < stdio.h>
 
 /**
  * main - Entry point
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf('_');
+	printf('p');
+	printf('u');
+	printf('t');
+	printf('c');
+	printf('h');
+	printf('a');
+	printf('r');
+	printf('\n');
 
 	return (0);
 }
